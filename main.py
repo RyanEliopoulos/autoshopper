8fa3e20899a17dd1b028810cc73ff0964c2967f3
Ryan <ryan.eliopoulos@gmail.com>
2021-01-22 16:57:05 -0800

    +auth code extraction
    Automated user consent process using selenium webdriver - entering
    credentials and extracting the authorization code from the redirect URI

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import requests
 import urllib.parse
+import os
+import time
+import re
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
+# Customer credentials
+username = os.getenv('kroger_username')
+password = os.getenv('kroger_password')
+
+
+# App credentials
 client_id: str = 'autoshopper-94b93de382bf39746138ed21e8405a5b4927188131458247672'
 client_secret: str = 'mEbTtN8lful792P3P0QNk06pKiCVADRaUV07dDn5'
 redirect_uri: str = 'http://138.68.18.203:8002/tictac/login'
@@ -9,7 +20,7 @@ redirect_uri: str = 'http://138.68.18.203:8002/tictac/login'
 api_base: str = 'https://api.kroger.com/v1/'
 
 
-params = {'scope': 'profile.compact, cart.basic:write, product.compact'
+params = {'scope': 'profile.compact cart.basic:write product.compact'
           , 'client_id': client_id
           , 'redirect_uri': redirect_uri
           , 'response_type': 'code'
@@ -23,20 +34,37 @@ def authorize_app() -> str:
     print(req.url)
     print(req.cookies)
 
-    # val = urllib.parse.urlencode(f'scope=profile.compact&client_id={client_id}&client_secret={client_secret}&redirect_uri={redirect_uri}&response_type=code&state=jomama')
     val = urllib.parse.urlencode(params)
     print(val)
 
-    # Consenting to my app
+    # Beginning authorization code process
     browser = webdriver.Firefox()
     browser.get(api_base + 'connect/oauth2/authorize?' + val)
 
-    # Copy/pasting "code" from the redirect uri
-    code = input('Whats the code?\n')
-    print(code)
+    # "Manually" consenting to app permissions
+    username_field = browser.find_element(By.ID, 'username')
+    password_field = browser.find_element(By.ID, 'password')
 
-    # Attempting to get a
+    username_field.send_keys(username)
+    time.sleep(1)
+    password_field.send_keys(password)
+    time.sleep(1)
+    password_field.send_keys(Keys.ENTER)
 
+    # Hacky wait since we don't know when the page will load
+    time.sleep(5)
+
+    # Now need to parse the url to extract the "code"
+    full_url = browser.current_url
+    print("fuller_url:" +full_url)
+    pattern = re.compile(r'\?code=(.*)&')
+    authorization_code = pattern.search(full_url).group(1)
+
+    return authorization_code
+
+
+
+auth_code = authorize_app()
 
 
 
