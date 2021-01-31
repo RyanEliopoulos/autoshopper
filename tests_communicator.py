d54c76a42d03528964e9f305bbc34379434bdd90
Ryan <ryan.eliopoulos@gmail.com>
2021-01-30 17:24:53 -0800

    + add_to_cart test

--- tests_communicator.py
+++ tests_communicator.py
@@ -4,7 +4,6 @@ from communicator import CustomerCommunicator
 
 
 class CustomerCommunicatorTests(unittest.TestCase):
-
     def test_get_authcode(self):
         customer_comm = CustomerCommunicator()
         customer_comm._get_authcode()
@@ -28,9 +27,24 @@ class CustomerCommunicatorTests(unittest.TestCase):
         values = customer_comm.get_productinfo(product_id)
         self.assertEqual(values['data']['productId'], product_id)
 
+    def test_add_to_cart(self):
+        # Prepping test
+        customer_comm = CustomerCommunicator()
+        customer_comm.get_tokens()
+        self.assertIsInstance(customer_comm.access_token, str)
+        self.assertIsInstance(customer_comm.refresh_token, str)
+        # Getting product info
+        product_id = '0021086500000'  # Heritage farms chicken thighs
+        values = customer_comm.get_productinfo(product_id)
+        self.assertEqual(values['data']['productId'], product_id)
+        upc = values['data']['upc']
+        # Building shopping list with newly acquired upc value
+        shopping_list = [{'upc': upc,  'quantity': 2}]
+        success: bool = customer_comm.add_to_cart(shopping_list)
+        self.assertEqual(success, True)
+
 
 class AppCommunicatorTests(unittest.TestCase):
-
     def test_get_tokens(self):
         app_comm = AppCommunicator()
         app_comm.get_tokens()
