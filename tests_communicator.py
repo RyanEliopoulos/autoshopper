32fe7d1a4ac206c37988515080fbc96f6f4098d0
Ryan <ryan.eliopoulos@gmail.com>
2021-02-02 23:00:24 -0800

    +tests for Communicator.product_search()
    Tests now checks for reasonable quantity of items in the response data
    as well as the presence of the search term as a substring in the product
    descriptions.

--- tests_communicator.py
+++ tests_communicator.py
@@ -43,6 +43,21 @@ class CustomerCommunicatorTests(unittest.TestCase):
         success: bool = customer_comm.add_to_cart(shopping_list)
         self.assertEqual(success, True)
 
+    def test_product_search(self):
+        search_term = "onion"  # must be lower case. Product descriptions are converted to lower.
+        # Prepping test
+        customer_comm = CustomerCommunicator()
+        customer_comm.get_tokens()
+        self.assertIsInstance(customer_comm.access_token, str)
+        self.assertIsInstance(customer_comm.refresh_token, str)
+        # Calling API
+        product_array = customer_comm.product_search(search_term)['data']
+        # Evaluating response data
+        self.assertTrue(len(product_array > 3))
+        for product in product_array:
+            descr_string = product['description'].lower()
+            self.assertTrue(search_term in descr_string)
+
 
 class AppCommunicatorTests(unittest.TestCase):
     def test_get_tokens(self):
@@ -60,3 +75,17 @@ class AppCommunicatorTests(unittest.TestCase):
         values = app_comm.get_productinfo(product_id)
         self.assertIsInstance(values, dict)
         self.assertEqual(values['data']['productId'], product_id)
+
+    def test_product_search(self):
+        search_term = "milk"  # must be lower case. Product descriptions are converted to lower.
+        # Prepping test
+        app_comm = AppCommunicator()
+        app_comm.get_tokens()
+        self.assertIsInstance(app_comm.access_token, str)
+        # Calling API
+        product_array = app_comm.product_search(search_term)['data']
+        # Evaluating response data
+        self.assertTrue(len(product_array) > 5)
+        for product in product_array:
+            descr_string = product['description'].lower()
+            self.assertTrue(search_term in descr_string)
