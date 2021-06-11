1ca3f2e4898d58c0513aef287d3e47350f6cb724
Ryan <ryan.eliopoulos@gmail.com>
2021-06-10 17:47:32 -0700

    +bugfix, recipe management
    bugfix: primary key for 'recipes' and 'recipe_ingredients' tables now
    auto increment.
    
    DBInterface now supports adding and retrieving recipes.

--- DBInterface.py
+++ DBInterface.py
@@ -1,4 +1,6 @@
 import sqlite3
+import Logger
+import copy
 
 
 class DBInterface:
@@ -8,6 +10,18 @@ class DBInterface:
         self.db_connection: sqlite3.Connection = sqlite3.connect(self.db_path, 10)
         self.db_connection.row_factory = sqlite3.Row
         self.db_cursor: sqlite3.Cursor = self.db_connection.cursor()
+
+    def manual_debug(self):
+        sqlstring: str = """ SELECT *
+                             FROM recipe_ingredients
+                         """
+        ret = self._execute_query(sqlstring)
+        if ret[0] != 0:
+            print(f'Error in manual debug' + ret[1])
+
+        results = self.db_cursor.fetchall()
+        for row in results:
+            print(tuple(row))
 
     def _execute_query(self, sql_string: str
                        , parameters: tuple = None) -> tuple[int, str]:
@@ -55,7 +69,7 @@ class DBInterface:
             return ret
         # Creating recipe tables
         sqlstring = """ CREATE TABLE recipes (
-                        recipe_id INT PRIMARY KEY,
+                        recipe_id INTEGER PRIMARY KEY NOT NULL,
                         recipe_title STRING NOT NULL,
                         recipe_notes STRING NOT NULL)
                     """
@@ -63,12 +77,12 @@ class DBInterface:
         if ret[0] != 0:
             return ret
         sqlstring = """ CREATE TABLE recipe_ingredients (
-                        ingredient_id INT PRIMARY KEY,
+                        ingredient_id INTEGER PRIMARY KEY,
                         ingredient_name STRING NOT NULL,
                         ingredient_quantity REAL NOT NULL,
                         ingredient_unit_type STRING NOT NULL,
-                        kroger_upc STRING,
-                        recipe_id INTEGER NOT NULL,
+                        kroger_upc STRING NOT NULL,
+                        recipe_id INT NOT NULL,
                         FOREIGN KEY(recipe_id) REFERENCES recipes(recipe_id))
                     """
         ret = self._execute_query(sqlstring)
@@ -134,3 +148,117 @@ class DBInterface:
             return ret
         self.db_connection.commit()
         return 0, 'Successfully updated refresh token'
+
+    def add_recipe(self, recipe: dict) -> tuple[int, dict]:
+        """
+        recipe:  {'recipe_title': str,
+                  'recipe_notes': str,
+                  'ingredients': {<ingredient1>: {'quantity': float,
+                                                 'ingredient_name': str,
+                                                 'unit_type': str,
+                                                 'kroger_upc': str},
+                                {<ingredient2>: {...} }
+                                }
+                 }
+        :return: A deep copy of recipe + the recipe and ingredient ids.
+        """
+        # Adding recipe table entry
+        recipe_title: str = recipe['recipe_title']
+        recipe_notes: str = recipe['recipe_notes']
+        sqlstring: str = """ INSERT INTO recipes (recipe_title, recipe_notes)
+                             VALUES (?, ?)
+                         """
+        ret: tuple = self._execute_query(sqlstring, (recipe_title, recipe_notes))
+        if ret[0] != 0:
+            Logger.Logger.log_error(f'Error adding {recipe_title} to database --' + ret[1])
+            print(f' Error adding new recipe {recipe_title} to the database')
+            return -1, {'error_message': f'Error adding {recipe_title} to the database--' + ret[1]}
+        new_recipe_id: int = self.db_cursor.lastrowid
+        # Adding ingredient table entries
+        for ingredient, details in recipe['ingredients'].items():
+            sqlstring = """ INSERT INTO recipe_ingredients
+                            (ingredient_name
+                            ,ingredient_quantity
+                            ,ingredient_unit_type
+                            ,kroger_upc
+                            ,recipe_id)
+                            VALUES (?, ?, ?, ?, ?)
+                        """
+            ret = self._execute_query(sqlstring, (details['ingredient_name'],
+                                                  details['quantity'],
+                                                  details['unit_type'],
+                                                  details['kroger_upc'],
+                                                  new_recipe_id))
+            if ret[0] != 0:
+                Logger.Logger.log_error(f'Error adding ingredient {ingredient} --' + ret[1])
+                print(f'Error adding ingredient {ingredient}')
+                # Returning without commit
+                self.db_connection.rollback()
+                return -1, {'error_message': f'Error adding ingredient {ingredient}--' + ret[1]}
+            new_ingredient_id: int = self.db_cursor.lastrowid
+            recipe['ingredients'][ingredient]['ingredient_id'] = new_ingredient_id
+        # Finalizing
+        self.db_connection.commit()
+        # Preparing return structure
+        new_recipe: dict = copy.deepcopy(recipe)
+        new_recipe['recipe_id'] = new_recipe_id
+        return 0, new_recipe
+
+    def get_recipes(self) -> tuple[int, dict]:
+        """
+        Pulls all recipe data and returns it as nested dictionaries
+        :return: [int: -1, {'error_message': <>}]
+                 [int: 0, {<recipe_title>: {'recipe_id': <>,
+                                            'recipe_title': <>,
+                                            'recipe_notes': <>,
+                                            'ingredients': {'ingredient_name': {<ingredient_name>,
+                                                                            'ingredient_quantity': float,
+                                                                            'ingredient_unit_type': str,
+                                                                            'kroger_upc': str
+                                                                            }, ...
+                                                            }
+                                            }
+                        }
+        """
+        sqlstring: str = """ SELECT 
+                             r.recipe_id
+                             ,r.recipe_title
+                             ,r.recipe_notes
+                             ,ri.ingredient_id
+                             ,ri.ingredient_name
+                             ,ri.ingredient_quantity
+                             ,ri.ingredient_unit_type
+                             ,ri.kroger_upc
+                             FROM recipes r, recipe_ingredients ri
+                             WHERE r.recipe_id = ri.recipe_id
+                             ORDER BY r.recipe_id
+                         """
+        ret: tuple = self._execute_query(sqlstring)
+        if ret[0] != 0:
+            return -1, {'error_message': ret[1]}
+        # Structuring data
+        new_recipe: dict = {}
+        recipes: dict = {}
+        results = self.db_cursor.fetchall()
+        for row in results:
+            new_ingredient: dict = {}
+            if row['recipe_id'] not in recipes:
+                # Initializing recipe
+                if len(recipes) > 0:
+                    new_recipe = dict()
+                new_recipe_title = row['recipe_title']
+                recipes[new_recipe_title] = new_recipe
+                new_recipe['recipe_id'] = row['recipe_id']
+                new_recipe['recipe_title'] = row['recipe_title']
+                new_recipe['recipe_notes'] = row['recipe_notes']
+                new_recipe['ingredients'] = dict()
+            # Adding ingredient info from current row
+            new_ingredient['ingredient_id'] = row['ingredient_id']
+            new_ingredient['ingredient_name'] = row['ingredient_name']
+            new_ingredient['ingredient_quantity'] = row['ingredient_quantity']
+            new_ingredient['ingredient_unit_type'] = row['ingredient_unit_type']
+            new_ingredient['kroger_upc'] = row['kroger_upc']
+            ingredient_name = new_ingredient['ingredient_name']
+            new_recipe['ingredients'][ingredient_name] = new_ingredient
+
+        return 0, recipes
